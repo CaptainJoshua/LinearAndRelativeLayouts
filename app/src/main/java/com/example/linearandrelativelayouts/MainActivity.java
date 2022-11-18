e210fc57234436f2144fd07268f404316b8e6323
CaptainJoshua <103025478+CaptainJoshua@users.noreply.github.com>
2022-11-19 05:27:13 +0800

    fix the layout_width of 4 side by side buttons

--- app/src/main/java/com/example/linearandrelativelayouts/MainActivity.java
+++ app/src/main/java/com/example/linearandrelativelayouts/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.linearandrelativelayouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         btnSwitch = findViewById(R.id.button12);
+
+            btnSwitch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    setContentView(R.layout.activity_main2);
+                }
+            });
+
     }
 }
--- app/src/main/java/com/example/linearandrelativelayouts/MainActivity2.java
+++ app/src/main/java/com/example/linearandrelativelayouts/MainActivity2.java
@@ -3,12 +3,24 @@ package com.example.linearandrelativelayouts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+ Button btnSwap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btnSwap = findViewById(R.id.button15);
+
+        btnSwap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               setContentView(R.layout.activity_main);
+            }
+        });
     }
 }
